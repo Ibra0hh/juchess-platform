9936e5843bd8f943f6a0a6be1009454c537b7fd2
Ibra0hh <285816952+Ibra0hh@users.noreply.github.com>
2026-07-17 19:07:48 +0300

    Remove handwritten JuChess wordmark font

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import '@fontsource/beth-ellen/latin-400.css'
 import './index.css'
 import App from './App.tsx'
 
